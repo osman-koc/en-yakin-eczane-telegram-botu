bb3a5a7ef5fe1d99a6b475a043ce27b95b6695d9
Osman Koç <kocosm@hotmail.com>
2024-07-05 13:26:20 +0300

    fix: openstreetmap-api bug resolved

--- api/openstreetmap-api.js
+++ api/openstreetmap-api.js
@@ -2,17 +2,25 @@ import 'dotenv/config';
 
 async function getCityAndDistrictFromLocation(latitude, longitude) {
     const locationUrl = `${process.env.OPENSTREETMAP_URI}?format=json&lat=${latitude}&lon=${longitude}&addressdetails=1`;
-    console.log('-> Request for LocationUrl: ' + locationUrl);
-
     const response = await fetch(locationUrl);
     const locationData = await response.json();
 
     if (!locationData.address) {
+        console.log('-> Request for LocationUrl: ' + locationUrl);
         throw new Error('Konum bilgisi alınamadı.');
     }
 
-    const city = locationData.address.district || locationData.address.city || locationData.address.village || locationData.address.province || '';
+    let city = locationData.address.province || locationData.address.district || locationData.address.city || locationData.address.village || '';
     const district = locationData.address.town || locationData.address.county || locationData.address.residential || '';
+
+    if (city === '' && district === '') {
+        console.log('-> Request for LocationUrl: ' + locationUrl);
+        throw new Error('Konum bilgisi alınamadı.');
+    }
+
+    if (city.includes(' ')) {
+        city = city.split(' ')[0];
+    }
 
     return { city, district };
 }

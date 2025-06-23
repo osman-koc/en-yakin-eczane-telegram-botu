135f5796e3188b6a5d4318d89c4eee55c8cfead8
osman-koc <kocosm@hotmail.com>
2025-06-23 14:57:04 +0300

    fix: find-pharmacies json

--- api/find-pharmacies.js
+++ api/find-pharmacies.js
@@ -3,12 +3,20 @@ import geolib from 'geolib';
 import queryString from 'query-string';
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
-const pharmaciesData = require('../db/pharmacies.json');
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Kullanıcı konumunu alıp il ve ilçeye göre eczaneleri bulan fonksiyon
 async function findPharmaciesFromDb(city, district, userLocation) {
     const upperCaseCity = city.toLocaleUpperCase('tr');
     const upperCaseDistrict = district.toLocaleUpperCase('tr');
+
+    // JSON dosyasını her çağrıldığında yeniden oku (doğru path ile)
+    const pharmaciesData = JSON.parse(fs.readFileSync(path.join(__dirname, '../db/pharmacies.json'), 'utf-8'));
 
     // Eczaneleri il ve ilçe bilgisine göre filtrele
     const filteredPharmacies = pharmaciesData.filter(pharmacy =>
@@ -22,7 +30,7 @@ async function findPharmaciesFromDb(city, district, userLocation) {
             latitude: parseFloat(pharmacy.location.lat),
             longitude: parseFloat(pharmacy.location.lon)
         };
-        console.log(pharmacyLocation);
+        //console.log(pharmacyLocation);
 
         const addressQuery = queryString.stringify({ query: pharmacy.address });
         const googleMapsUrl = `${process.env.GOOGLE_MAPS_URI}&${addressQuery}`;
